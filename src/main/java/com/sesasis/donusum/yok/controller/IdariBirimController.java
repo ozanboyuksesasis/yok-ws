94f59a605f10c82017f8e5cb77a195f8c506aada
emre9ybar-sesasis <emre.aybar@sesasis.com>
2024-10-21 18:49:59 +0300

    [NO-4]- idari birim entity

---
+++ src/main/java/com/sesasis/donusum/yok/controller/IdariBirimController.java
@@ -0,0 +1,43 @@
+package com.sesasis.donusum.yok.controller;
+
+import com.sesasis.donusum.yok.constant.MappingConstants;
+import com.sesasis.donusum.yok.core.payload.ApiResponse;
+import com.sesasis.donusum.yok.dto.IdariBirimDTO;
+import com.sesasis.donusum.yok.service.IdariBirimService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/idaribirim")
+@RequiredArgsConstructor
+public class IdariBirimController {
+
+    private final IdariBirimService idariBirimService;
+
+    @PostMapping(value = "save",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> save(@RequestBody IdariBirimDTO idariBirimDTO) {
+        ApiResponse response = idariBirimService.save(idariBirimDTO);
+        return ResponseEntity.ok(response);
+    }
+
+
+    @GetMapping(value = "all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> all(IdariBirimDTO idariBirimDTO) {
+        return ResponseEntity.ok(idariBirimService.findAll());
+    }
+
+    @GetMapping(value = "{/id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> one(@PathVariable Long id) {
+        return ResponseEntity.ok(idariBirimService.findById(id));
+    }
+
+    @DeleteMapping(MappingConstants.PATH_VARIABLE_ID)
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        idariBirimService.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+
+}
---
+++ src/main/java/com/sesasis/donusum/yok/service/IdariBirimService.java
@@ -0,0 +1,70 @@
+package com.sesasis.donusum.yok.service;
+
+import com.sesasis.donusum.yok.core.payload.ApiResponse;
+import com.sesasis.donusum.yok.core.service.IService;
+import com.sesasis.donusum.yok.dto.IdariBirimDTO;
+import com.sesasis.donusum.yok.entity.IdariBirim;
+import com.sesasis.donusum.yok.mapper.ModelMapperServiceImpl;
+import com.sesasis.donusum.yok.repository.IdariBirimRepository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.parameters.P;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Service
+public class IdariBirimService implements IService<IdariBirimDTO> {
+
+    private IdariBirimRepository idariBirimRepository;
+    private ModelMapperServiceImpl modelMapperServiceImpl;
+
+    @Override
+    public ApiResponse save(IdariBirimDTO idariBirimDTO) {
+        IdariBirim idariBirim = this.modelMapperServiceImpl.request().map(idariBirimDTO, IdariBirim.class);
+        ApiResponse validationResponse = saveControl(idariBirimDTO);
+        if (!validationResponse.getSuccess()) {
+            return validationResponse;
+        }
+        idariBirimRepository.save(idariBirim);
+        return new ApiResponse(true, "İşlem başarılı.", null);
+    }
+
+    public ApiResponse saveControl(IdariBirimDTO idariBirimDTO) {
+        if (idariBirimDTO.getName().trim() == null || idariBirimDTO.getName().trim().isEmpty()) {
+            return new ApiResponse<>(false, "Hata: İsim boş olamaz.", null);
+        }
+        return new ApiResponse(true, "İşlem başarılı.", null);
+    }
+
+    @Override
+    public ApiResponse findAll() {
+        List<IdariBirim> birims = idariBirimRepository.findAll();
+        List<IdariBirimDTO> dtos = birims.stream().map(birim ->
+                this.modelMapperServiceImpl.response().map(birim, IdariBirimDTO.class)).collect(Collectors.toList());
+        return new ApiResponse(true, "İşlem başarılı.", dtos);
+    }
+
+    @Override
+    public ApiResponse findById(Long id) {
+        IdariBirim idariBirim = idariBirimRepository.findById(id).orElse(null);
+        if (idariBirim == null) {
+            return new ApiResponse(true, "İdari birim bulunamadı.", null);
+        }
+        IdariBirimDTO idariBirimDTO = this.modelMapperServiceImpl.response().map(idariBirim, IdariBirimDTO.class);
+        return new ApiResponse<>(true, "İşlem başarılı.", idariBirimDTO);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        if (idariBirimRepository.existsById(id)) {
+            idariBirimRepository.deleteById(id);
+        }
+
+    }
+}
