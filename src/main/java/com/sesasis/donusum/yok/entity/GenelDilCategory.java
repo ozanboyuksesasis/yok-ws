2b5d0ecf9931d2ad6982e4c02a2ece7a147a8a16
emre9ybar <aybaremre5@gmail.com>
2024-11-25 17:01:06 +0300

    Update

--- src/main/java/com/sesasis/donusum/yok/entity/GenelDilCategory.java
+++ src/main/java/com/sesasis/donusum/yok/entity/GenelDilCategory.java
@@ -11,7 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@Table(name = "haber_dil_category")
+@Table(name = "genel_dil_category")
 public class GenelDilCategory {
 
     @Id
