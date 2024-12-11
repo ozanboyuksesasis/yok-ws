61be726b4b49429d5cb7d4d355fec7134ca486de
emre9ybar <aybaremre5@gmail.com>
2024-12-11 14:29:08 +0300

    [NO-61] - Alt menü - baslik servis düzeltme.

--- src/main/java/com/sesasis/donusum/yok/repository/AnaBaslikRepository.java
+++ src/main/java/com/sesasis/donusum/yok/repository/AnaBaslikRepository.java
@@ -3,8 +3,7 @@ package com.sesasis.donusum.yok.repository;
 import com.sesasis.donusum.yok.entity.AnaBaslik;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface AnaBaslikRepository extends JpaRepository<AnaBaslik, Long> {
     AnaBaslik findOneByDomainId(Long domainId);
+    boolean existsById(Long domainId);
 }
