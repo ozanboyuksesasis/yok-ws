e9cd419f325fe7fdfeb7972ca8f517ba264c5bab
halidAcarSesasis <halid.acar@sesasis.com>
2024-12-17 15:15:17 +0300

    anadomain icin slider haber ekleme yapildi

---
+++ src/main/java/com/sesasis/donusum/yok/repository/AnaDomainSliderRepository.java
@@ -0,0 +1,7 @@
+package com.sesasis.donusum.yok.repository;
+
+import com.sesasis.donusum.yok.entity.AnaDomainSlider;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnaDomainSliderRepository extends JpaRepository<AnaDomainSlider, Long> {
+}
